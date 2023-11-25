d26011cb0336227c1e26245fb9ba0cbc48258847
Peter Marshall <marshall.peter@gmail.com>
2023-11-25 15:03:56 +0000

    working notification

--- spout/src/main.rs
+++ spout/src/main.rs
@@ -1,7 +1,13 @@
-use notify_rust::Notification;
+use notify_rust::{Notification, Hint};
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    Notification::new().summary("minimal notification").show()?;
+    Notification::new()
+    .summary("minimal notification")
+    .body("This has nothing to do with emails.\nIt should not go away until you acknowledge it.")
+    .icon("thunderbird")
+    .appname("thunderbird")
+    .timeout(0)
+    .show()?;
     Ok(())
 }
