3269e60a4ac76087e0dc2202002926b86d7f5e87
Peter Marshall <marshall.peter@gmail.com>
2023-11-25 18:27:33 +0000

    added in code to read a file

--- spout/src/main.rs
+++ spout/src/main.rs
@@ -1,7 +1,13 @@
+use std::env;
 use notify_rust::{Notification, Hint};
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // get the command line arguments
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
+
     Notification::new()
     .summary("minimal notification")
     .body("This has nothing to do with emails.\nIt should not go away until you acknowledge it.")
@@ -9,5 +15,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     .appname("thunderbird")
     .timeout(0)
     .show()?;
-    Ok(())
+
+
+
+
+
+
+    let mut file = File::open(path).await.unwrap();
+    let mut interval = time::interval(Duration::from_millis(1000));
+    let mut contents = vec![];
+    let mut position = 0;
+    
+    loop {
+        contents.truncate(0);
+        file.seek(SeekFrom::Start(position as u64)).await;
+        position += file.read_to_end(&mut contents).await.unwrap();
+        
+        /// do_process(contents)
+        
+        interval.tick().await;
+    }
+
+    Ok(());
 }
+
+//https://stackoverflow.com/questions/71632833/how-to-continuously-watch-and-read-file-asynchronously-in-rust-using-tokio
